33d1ea11596a48c0d8f4437a9de581932a0a8d14
Emmanuel <manuboisset@gmail.com>
2023-02-11 16:24:51 +0100

    algo complete without crash

--- client/src/md5_hashcash.rs
+++ client/src/md5_hashcash.rs
@@ -1,6 +1,6 @@
-use std::ptr::null;
-use md5::digest::Output;
-use md5::Md5;
+use md5::{Md5, Digest};
+use std::{str};
+use std::fmt::Write;
 use common::structs::{MD5HashCashInput, MD5HashCashOutput, ChallengeTrait};
 
 pub struct MD5HashCash {
@@ -17,9 +17,11 @@ fn hash(message: &str) -> String{
 
 fn count_bits_zero(string: &str) -> u64{
 
-    let binary = str::from_utf8(&hex::decode(string).expect("Erreur au decoding de la chaine")).expect("Erreur de conversion");
-
     let mut count = 0;
+    let decode = hex::decode(string).expect("Erreur au decoding de la chaine");
+    let binary =
+        str::from_utf8(&decode).expect("Erreur de conversion");
+
     for i in binary.chars() {
         if i == '0' {
             count += 4;
@@ -47,26 +49,32 @@ impl ChallengeTrait for MD5HashCash {
     }
 
     fn new(_input: Self::Input) -> Self {
-        MD5HashCash{ _input, _output: Null }
+        MD5HashCash{ _input, _output: MD5HashCashOutput{ seed: 0, hashcode: "".to_string() } }
     }
 
     fn solve(&self) -> Self::Output {
         let seed: u64 = 0;
+        let mut hex_seed = String::new();
         write!(&mut hex_seed, "{:016x}", seed).expect("Erreur à la conversion du seed en hexa");
-        let hash = hash(&(hex_seed + &*_input.message));
-        let mut bits_to_zero = count_bits_zero(&hash);
-        let _input: MD5HashCashInput = Self::Input;
+        let mut hash_code = hash(&(hex_seed + &self._input.message));
+        let mut bits_to_zero = count_bits_zero(&hash_code);
+        let _input: &MD5HashCashInput = &self._input;
         while bits_to_zero < _input.complexity as u64 {
-            bits_to_zero = count_bits_zero(&hash);
+            let seed: u64 = seed + 1;
+            let mut hex_seed = String::new();
+            write!(&mut hex_seed, "{:016x}", seed).expect("Erreur à la conversion du seed en hexa");
+            hash_code = hash(&(hex_seed + &self._input.message));
+            bits_to_zero = count_bits_zero(&hash_code);
         }
-        &self._output.seed = &seed;
-        &self._output.hashcode = &hash;
-        Self::Output = &self._output;
-        return Output
+        let output = MD5HashCashOutput{ seed, hashcode: hash_code };
+        output
+        /*&self._output.unwrap().seed = output.seed;
+        &self._output.unwrap().hashcode = output.hashcode.to_string();
+        self._output.unwrap()*/
     }
 
     fn verify(&self, _output: &Self::Output) -> bool {
-        return if &self._output == _output {
+        return if &self._output.seed == &_output.seed {
             true
         } else { false }
     }

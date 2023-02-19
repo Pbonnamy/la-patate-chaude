bada9b95c674e7d0f37eef7a8110e64978f26fba
Emmanuel <manuboisset@gmail.com>
2023-02-19 16:07:42 +0100

    fix algo md5 hash

--- client/src/md5_hashcash.rs
+++ client/src/md5_hashcash.rs
@@ -40,7 +40,7 @@ fn verifyHashCode(message: &str, seed: u64) -> Result<String, Error>{
     let seed: u64 = seed;
     let mut hex_seed = String::new();
     write!(&mut hex_seed, "{:016x}", seed).expect("Erreur à la conversion du seed en hexa");
-    let hash_code = hash(&(hex_seed + &message));
+    let hash_code = hash(&(hex_seed.to_uppercase().clone() + &message));
     return Ok(hash_code.to_uppercase());
 }
 
@@ -64,26 +64,26 @@ impl ChallengeTrait for MD5HashCash {
         let mut seed: u64 = 0;
         let mut hex_seed = String::new();
         write!(&mut hex_seed, "{:016x}", seed).expect("Erreur à la conversion du seed en hexa");
-        let mut hash_code = hash(&(hex_seed + &self._input.message));
+        let mut hex_hash = hex_seed.to_uppercase().clone();
+        let mut hash_code = hash(&(hex_seed.to_uppercase().clone() + &self._input.message));
         let mut bits_to_zero = count_bits_zero(&hash_code);
         let input: &MD5HashCashInput = &self._input;
         while bits_to_zero < input.complexity as u64 {
             seed += 1;
             let mut hex_seed = String::new();
             write!(&mut hex_seed, "{:016x}", seed).expect("Erreur à la conversion du seed en hexa");
-            hash_code = hash(&(hex_seed + &self._input.message));
+            hex_hash = hex_seed.to_uppercase().clone();
+            hash_code = hash(&(hex_seed.to_uppercase().clone() + &self._input.message));
             bits_to_zero = count_bits_zero(&hash_code);
         }
+        //println!("hash: {}", hash_code);
+        //println!("hash: {}", hex_hash);
         let output = MD5HashCashOutput{ seed, hashcode: hash_code.to_uppercase() };
-
         if self.verify(&output) {
             return output;
         } else {
             panic!("Erreur dans la vérification du hashcash")
         }
-        /*&self._output.unwrap().seed = output.seed;
-        &self._output.unwrap().hashcode = output.hashcode.to_string();
-        self._output.unwrap()*/
     }
 
     fn verify(&self, _output: &Self::Output) -> bool {

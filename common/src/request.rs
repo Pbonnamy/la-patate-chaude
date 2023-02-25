b7f3879dcc12d6a3c9e5c2f43c154e7ec56d4dd5
Pierre <bonnamy.77@gmail.com>
2023-02-25 17:33:39 +0100

    check formatting

--- common/src/request.rs
+++ common/src/request.rs
@@ -4,7 +4,10 @@ use std::net::TcpStream;
 
 pub fn send_message(stream: &mut TcpStream, msg: Message) {
     let json = serde_json::to_string(&msg).unwrap();
-    println!("\n\x1b[34mDEBUG (Sended) : {}\x1b[0m", serde_json::to_string_pretty(&msg).unwrap());
+    println!(
+        "\n\x1b[34mDEBUG (Sended) : {}\x1b[0m",
+        serde_json::to_string_pretty(&msg).unwrap()
+    );
     let len = json.len() as u32;
 
     stream.write(&len.to_be_bytes()).unwrap();
@@ -22,7 +25,7 @@ pub fn receive_message(stream: &mut TcpStream) -> Message {
 
     let json = serde_json::from_str(&res).unwrap();
 
-    println!("\n\x1b[31mDEBUG (Received) : {}\x1b[0m", serde_json::to_string_pretty(&json).unwrap());
+    println!("\n\x1b[31mDEBUG (Received) : {}\x1b[0m",serde_json::to_string_pretty(&json).unwrap());
 
     return json;
 }

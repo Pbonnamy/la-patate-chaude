d5ff84d12c8a384d5db4244fcbb81a639a4e552a
Emmanuel <manuboisset@gmail.com>
2023-02-27 17:48:21 +0100

    add rustdoc + add tests

--- client/src/md5_hashcash.rs
+++ client/src/md5_hashcash.rs
@@ -1,12 +1,27 @@
 use common::structs::{ChallengeTrait, MD5HashCashInput, MD5HashCashOutput};
 use md5::{Digest, Md5};
 
+/// A challenge that requires the solver to find a seed that produces a hashcode with a given number of leading zeros.
+/// Le challenge consiste à trouver un seed qui produit un hashcode avec un nombre donné de zéros en tête.
+/// Las structure est définit de la manière suivante
 pub struct MD5HashCash {
+    /// L'input du challenge est de type MD5HashCashInput
     input: MD5HashCashInput,
     #[allow(dead_code)]
+    /// L'output du challenge est de type MD5HashCashOutput
     output: MD5HashCashOutput,
 }
 
+/// Fonction qui calcule le hashcode d'un message
+/// La fonction prend en paramètre un message de type String
+/// # Arguments
+/// * `message` - Le message à hasher
+/// # Example
+/// ```
+/// let hashcode = hash("0000000000000131Our boring client talks to a red computer.");
+/// ```
+/// # Output
+/// Un hashcode de type String qui est le résultat du hashage du message en hexadécimal
 fn hash(message: String) -> String {
     let mut hasher = Md5::new();
     hasher.update(message);
@@ -17,15 +32,30 @@ fn hash(message: String) -> String {
     hashcode
 }
 
+/// Challenge Md5HashCash qui implémente le trait ChallengeTrait
 impl ChallengeTrait for MD5HashCash {
+    /// Le type d'input est MD5HashCashInput
     type Input = MD5HashCashInput;
 
+    /// Le type d'output est MD5HashCashOutput
+    /// # Exemple
+    /// ```
+    /// fn solve(&self) -> Self::Output{ return self.output;}
+    /// ```
     type Output = MD5HashCashOutput;
 
     fn name() -> String {
         String::from("HashCash")
     }
 
+    /// Fonction qui retourne une nouvelle instance de MD5HashCash
+    /// # Arguments
+    /// * `input` - Le message à hasher + la complexité du challenge de type MD5HashCashInput
+    /// # Exemple
+    /// ```
+    /// use md5_hashcash::MD5HashCash;
+    /// let challenge = MD5HashCash::new(input);
+    /// ```
     fn new(input: Self::Input) -> Self {
         MD5HashCash {
             input,
@@ -36,6 +66,15 @@ impl ChallengeTrait for MD5HashCash {
         }
     }
 
+    /// Fonction qui résout le challenge
+    /// # Exemple
+    /// ```
+    /// use md5_hashcash::MD5HashCash;
+    /// let challenge = MD5HashCash::new(input);
+    /// let output = challenge.solve();
+    /// ```
+    /// # Output
+    /// Le seed qui a permis de trouver le hashcode avec la complexité demandée de type MD5HashCashOutput
     fn solve(&self) -> Self::Output {
         let input = self.input.message.clone();
         let mut seed = 0;
@@ -61,6 +100,18 @@ impl ChallengeTrait for MD5HashCash {
         output
     }
 
+    /// Fonction qui vérifie si le hashcode trouvé est valide
+    /// # Arguments
+    /// * `output` - Le hashcode trouvé de type MD5HashCashOutput
+    /// # Exemple
+    /// ```
+    /// use md5_hashcash::MD5HashCash;
+    /// let challenge = MD5HashCash::new(input);
+    /// let output = challenge.solve();
+    /// let is_valid = challenge.verify(&output);
+    /// ```
+    /// # Output
+    /// True si le hashcode trouvé est valide, False sinon
     fn verify(&self, output: &Self::Output) -> bool {
         let hex_integer = u128::from_str_radix(&output.hashcode, 16).unwrap();
         let leading_zero = hex_integer.leading_zeros();
@@ -84,4 +135,26 @@ mod tests {
         let output = hash_cash.solve();
         assert!(hash_cash.verify(&output));
     }
+
+    #[test]
+    fn test_hex_seed(){
+        let seed = 789;
+        let hex_seed = format!("{:016X}", seed);
+        assert_eq!(hex_seed, "0000000000000315");
+    }
+
+    #[test]
+    fn test_hash(){
+        let message = "Hello World".to_string();
+        let hashcode = super::hash(message);
+        assert_eq!(hashcode, "B10A8DB164E0754105B7A99BE72E3FE5");
+    }
+
+    #[test]
+    fn test_count_bits_zero(){
+        let hex_integer = u128::from_str_radix("ED076287532E86365E841E92BFC50D8C", 16).unwrap();
+        let leading_zero = hex_integer.leading_zeros();
+        assert_eq!(leading_zero, 0);
+    }
+
 }
